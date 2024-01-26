1cde96ab5ce4817e5a3e0b8c060ddbc0d6fe7aee
José Galisteo <ceritium@gmail.com>
2024-01-26 17:51:17 +0100

    Some improvements

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
-import time #Módulo que nos permite darte tiempo de lectura al lector con método time.sleep(segundos)
-
+import time # Módulo que nos permite darte tiempo de lectura al lector con método time.sleep(segundos)
+import inquirer # pip install inquirer
+import re
+import argparse
 
 """
 
@@ -17,8 +19,12 @@ Estas funciones nos sirven para iniciar la presentación de la aventura y crear 
 
 """
 
-def linebreaker(): #Se usa para separar visualmente episodios del juego
+def wait():
+    if fast == False:
         time.sleep(1)
+
+def linebreaker(): #Se usa para separar visualmente episodios del juego
+        wait()
         print(" ")
         x = " *** "
         print(x*15)
@@ -27,42 +33,15 @@ def linebreaker(): #Se usa para separar visualmente episodios del juego
 
 
 def introduccion(): #Texto de narrativa: presentación de juego
-    time.sleep(1)
+    wait()
     print("Bienvenido a Dungeons&Pythons. Estás a punto de embarcarte en una emocionante aventura.")
     print(" ")
-    time.sleep(1)
+    wait()
     print("Como aventurero, podrás ponerte a prueba como héroe y ganar renombre en una tierra inhóspita")
     print(" ")
-    time.sleep(1)
+    wait()
     print("Pero primero, necesitarás decirme quien eres para poder dar tu primer paso...")
     print(" ")
-
-
-def name(): #Dota al personaje de nombre
-    nombre = input("¿Cuál es tu nombre?: ").capitalize()
-    return nombre
-
-
-
-def race(razas): #Dota al personaje de raza jugable. Según la raza cambia las opciones de juego(aunque en demo no implementa)
-    while True:
-            raza = input("Elige tu raza: human@, elf@, enan@: ").lower()
-            if raza in razas:
-                break
-            else:
-                print(f"No contemplamos {raza} como raza jugable por ahora")
-    return raza
-
-
-
-def profession(profesiones): #Dota a personaje de profesión jugable. Determina opciones de narrativa e inventario
-    while True:
-        profesion = input("Elige tu profesión: guerrer@, hechicer@, pícar@: ").lower()
-        if profesion in profesiones:
-            break
-        else:
-            print(f"No contemplamos {profesion} como profesión jugable por ahora")
-    return profesion
 
 
 """
@@ -70,34 +49,33 @@ Estas funciones lanzan textos de narrativa personalizando el juego según opcion
 
 """
 
-
 def inicio_raza(): #Texto de narrativa: origen del personaje según raza
     print(f"Saludos {nombre}, valiente {raza} que busca fortuna como {profesion}")
 
-    if raza == "humano" or raza == "humana":
-            time.sleep(1)
+    if re.match(r'.*human.*', raza):
+            wait()
             (" ")
             print("Vienes de la capital del Reino, con la idea de derrotar al dragón del lago")
-    elif raza == "elfo" or raza == "elfa":
-            time.sleep(1)
+    elif re.match(r'.*elf.*', raza):
+            wait()
             (" ")
             print("Vienes desde lo más profundo del bosque, con la idea de derrotar al dragón del lago")
-    elif raza == "enano" or raza == "enana":
+    elif re.match(r'.*enan.*', raza):
             print(" ")
-            time.sleep(1)
+            wait()
             print("Vienes desde las entrañas de la montaña, con la idea de derrotar al dragón del lago")
 
 def inicio_profesion(): #Texto de narrativa: contexto según profesión
-    if profesion == "guerrero" or profesion == "guerrera":
-            time.sleep(1)
+    if re.match(r'.*guerrer.*', raza):
+            wait()
             print(" ")
             print("Piensas que con tu fuerza, podrás hacer frente a la amenaza que se cierne sobre tu pueblo")
-    elif profesion == "hechicero" or profesion == "hechicera":
-            time.sleep(1)
+    elif re.match(r'.*hechicer.*', raza):
+            wait()
             print(" ")
             print("Piensas que con tu magia, te valdrás para contener este peligro que amenaza a los tuyos")
-    elif profesion == "pícaro" or profesion == "pícara":
-            time.sleep(1)
+    elif re.match(r'.*picar.*', raza):
+            wait()
             print(" ")
             print("Confías en que tus artimañas sean suficientes para cobrar la recompensa por el monstruo")
 
@@ -105,19 +83,17 @@ def inicio_profesion(): #Texto de narrativa: contexto según profesión
 Estas funciones permiten avanzar en la narrativa y crear el arma como primer objeto adquirido por el personaje
 
 """
-
-
 def interludio(): #Texto de narrativa: trama continua
-    time.sleep(1)
+    wait()
     print("El camino hacia el lago te lleva varíos días, atravesando toda clase que parajes")
     print(" ")
-    time.sleep(1)
+    wait()
     print("El dragón ha causado muchos daños allá por donde pasas. Toda clase de parajes han sido reducidos a cenizas. Es una amenaza que ha de ser eliminada con premura")
     print(" ")
-    time.sleep(1)
+    wait()
     print("Antes de alcanzarlo, tienes que asegurarte y revisar tu inventario. Sin la adecuada preparación, seguramente morirás")
     print(" ")
-    time.sleep(1)
+    wait()
 
 def weapon(profesion): #Crea una biblioteca con un elemento {profesión:arma}
 
@@ -130,7 +106,7 @@ def weapon(profesion): #Crea una biblioteca con un elemento {profesión:arma}
         arma[profesion] = "arco y flechas"
 
     print(f"Recuerdas qué trajiste tu inseparable {arma[profesion]} contigo y crees que tu maestría en su manejo te será suficiente para derrotar a la bestia")
-    time.sleep(1)
+    wait()
     print(" ")
     return arma
 
@@ -143,13 +119,13 @@ u otra en el desenlace de la aventura
 
 
 def antes_del_lago(): #Texto de narrativa: avanza la trama
-    time.sleep(1)
+    wait()
     print(" ")
     print("Llegas al Bosque del Descanso, una arboleda cercana al lago del dragón. Un lugar anteriormente frondoso que ha sido reducido a cenizas casi en su totalidad")
-    time.sleep(1)
+    wait()
     print(" ")
     print("Solo la arboleda te separa de tu encuentro inevitable con tu monstruoso némesis. Quizás sea buena idea buscar aquí algún sitio para descansar; o puede que te sientas con energías para continuar tu viaje y acabar con todo esto cuanto antes")
-    time.sleep(1)
+    wait()
 
 def descanso(): #Elección de camino. Permite actualizar el inventario
     print(" ")
@@ -186,16 +162,16 @@ desenlace de la aventura tendrá un final u otro
 
 
 def encuentro_dragon(): #Texto de narrativa: introducción al encuentro final
-    time.sleep(1)
+    wait()
     print(" ")
     print("Sigues caminando toda la noche hacia el lago. Durante horas has olido el azufre que despide el dragón y a cada paso que das, se intensifica.")
-    time.sleep(1)
+    wait()
     print(" ")
     print("Pasan horas y horas, pero tu estado de alerta hacen que parezcan solo minutos.")
-    time.sleep(1)
+    wait()
     print(" ")
     print("Cuando te quieres dar cuenta, ya estás a los pies de la gigantesca charca en donde habita la bestia")
-    time.sleep(1)
+    wait()
 
 
 def antes_del_combate(): #Permite acceder a la ficha de personaje si se desea o continuar tal cual
@@ -208,7 +184,7 @@ def antes_del_combate(): #Permite acceder a la ficha de personaje si se desea o 
         try:
             opcion = int(input("¿Qué harás?: "))
             if opcion == 1:
-                time.sleep(1)
+                wait()
                 print(" ")
                 print(f"Nombre: {nombre}")
                 print(f"Raza: {raza.capitalize()}")
@@ -218,40 +194,40 @@ def antes_del_combate(): #Permite acceder a la ficha de personaje si se desea o 
                 print(" ")
                 break
             elif opcion == 2:
-                time.sleep(1)
+                wait()
                 print(" ")
                 print(f"Bueno {nombre}, si crees que no necesitas revisar vamos allá. ¡El mundo pertenece a los arrojados!")
                 break
             else:
-                time.sleep(1)
+                wait()
                 print(" ")
                 print("Sé que el agotamiento te distrae, pero tienes que elegir: 1 o 2 ")
-                time.sleep(1)
+                wait()
                 print(" ")
                 opcion = int(input("¿Qué harás? "))
         except ValueError:
-                    time.sleep(1)
+                    wait()
                     print(" ")
                     print("Sé que el agotamiento te distrae, pero tienes que elegir: 1 o 2 ")
 
 
 
 def combate_final(): #Elección de acciones: suma de todas las elecciones hechas anteriores para dar dos finales: ganar o vencer
-    time.sleep(1)
+    wait()
     print(" ")
     print("Sientes que es el momento, así que gritas al cielo: ¡DRAGON! ¡HE VENIDO A POR TI")
-    time.sleep(1)
+    wait()
     print(" ")
     print("De las profundidades del lago, emerge la criatura. Roja como la sangre, grande como un castillo")
-    time.sleep(1)
+    wait()
     print(" ")
     print("Sin tiempo para vacilar, se avalanza hacia ti. ¡Es el momento de la verdad!")
-    time.sleep(1)
+    wait()
     (" ")
     print(f"Pulsa 1 si quieres usar tu {arma[profesion]} contra la criatura")
     print("Pulsa 2 si decides esquivar el ataque del dragón y luego contraatacar")
 
-    time.sleep(1)
+    wait()
     print(" ")
     x = " *** "
     print(x*15)
@@ -262,84 +238,112 @@ def combate_final(): #Elección de acciones: suma de todas las elecciones hechas
         try:
             opcion = int(input("¿Qué harás? "))
             if opcion == 1:
-                time.sleep(1)
+                wait()
                 (" ")
                 print(f"Te ensarzas en una batalla épica con la criatura. La lucha dura una eternidad. Tu {arma[profesion]} contra sus fauces y colmillos")
-                time.sleep(1)
+                wait()
                 (" ")
                 print(f"Tus energías se agotan y crees que la derrota es inminente")
-                time.sleep(1)
+                wait()
                 (" ")
                 print(f"Sin embargo, tu experiencia como {profesion} te permite al final del todo encontrar un punto débil")
                 if profesion == "guerrero" or profesion == "guerrera":
-                                                     time.sleep(1)
+                                                     wait()
                                                      (" ")
                                                      print(f"En un momento de debilidad del enemigo, clavas tu espada entre sus escamas alcanzado su corazón")
-                                                     time.sleep(1)
+                                                     wait()
                                                      (" ")
                                                      print("¡Enhorabuena! ¡Has vencido al dragón!")
                                                      break
                 elif profesion == "hechicero" or profesion == "hechicera":
-                                                     time.sleep(1)
+                                                     wait()
                                                      (" ")
                                                      print(f"En un momento de debilidad del enemigo, lanzas una bola de fuego que derrite las escamas de su pecho, calcinando su corazón")
-                                                     time.sleep(1)
+                                                     wait()
                                                      (" ")
                                                      print("¡Enhorabuena! ¡Has vencido al dragón!")
                                                      break
                 elif profesion == "pícaro" or profesion == "pícara":
-                                                     time.sleep(1)
+                                                     wait()
                                                      (" ")
                                                      print(f"En un momento de debilidad del enemigo, te escabulles entre sus garras para lanzar una flecha que se clava directamente en su corazón")
-                                                     time.sleep(1)
+                                                     wait()
                                                      (" ")
                                                      print("¡Enhorabuena! ¡Has venido al dragón!")
                                                      break
             elif opcion == 2:
                     valor = "pócima"
                     if valor in inventario:
-                                         time.sleep(1)
+                                         wait()
                                          (" ")
                                          print("Esquivas el ataque pero las garras del dragón te rozan mortalmente")
-                                         time.sleep(1)
+                                         wait()
                                          (" ")
                                          print("Por suerte, puedes usar la poción que encontraste en la cueva y te repones")
-                                         time.sleep(1)
+                                         wait()
                                          (" ")
                                          print("Tomas aire... y lanzas tu contraataque")
                     else:
-                         time.sleep(1)
+                         wait()
                          (" ")
                          print("Esquivas el ataque pero las garras del dragón te rozan mortalmente... que bien te habría venido una poción de salud ahora mismo")
-                         time.sleep(1)
+                         wait()
                          (" ")
                          print("Caes al suelo, malherido... Lo último que ves son las fauces del monstruo acercandose, amenazante. Hoy serás su cena.")
-                         time.sleep(1)
+                         wait()
                          (" ")
                          print("¡Lo siento! ¡Has sido devorado por la bestia y ahora tu pueblo está condenado")
                          break
             else:
-                time.sleep(1)
+                wait()
                 print(" ")
                 print("Sé que el agotamiento te distrae, pero tienes que elegir: 1 o 2 ")
-                time.sleep(1)
+                wait()
                 print(" ")
                 opcion = int(input("¿Qué harás? "))
         except ValueError:
-                    time.sleep(1)
+                    wait()
                     print(" ")
                     print("Sé que el agotamiento te distrae, pero tienes que elegir: 1 o 2 ")
+
+
+def createPlayer():
+    questions = [
+        inquirer.Text("nombre", message="¿Cuál es tu nombre?"),
+        inquirer.List(
+            "raza",
+            message="Elige tu raza",
+            choices=razas
+        ),
+        inquirer.List(
+            "profesion",
+            message="Elige tu profesión",
+            choices=profesiones
+        ),
+    ]
+
+    answers = inquirer.prompt(questions)
+    nombre = answers["nombre"]
+    raza = answers["raza"]
+    profesion = answers["profesion"]
+    return nombre, raza, profesion
 
 """
 Jerarquía de funciones para hacer correr la aventura
 
 """
 
+parser = argparse.ArgumentParser(
+                    prog='Dugeons&Pythons',
+                    description='Juego de aventuras en Python',
+                    epilog='¡Que te diviertas!')
+parser.add_argument('-f', '--fast', action='store_true', help='No espera entre escenas')
+args = parser.parse_args()
+fast = args.fast
+
 introduccion()
 linebreaker()
-nombre = name()
-raza = race(razas)
-profesion = profession(profesiones)
+nombre, raza, profesion = createPlayer()
 linebreaker()
 inicio_raza()
 inicio_profesion()
